02d04d574971b7853bf74ed590bb771a1b4ee0a0
bichhanhh146-max <bichhanhh146@gmail.com>
2025-08-29 00:06:35 +0700

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1rpd-iISPiVhElvbU1a4TO64NP03BsfVO
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import numpy as np
